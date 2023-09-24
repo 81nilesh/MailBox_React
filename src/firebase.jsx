d23cd2b1619699bb679e533988bb32a67dabb80d
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-24 13:58:47 +0530

    firebase commit

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHVOQ2z1ZItPXvHj_EVvK-0h5LVXt6c4E",
+  authDomain: "mailbox-96a99.firebaseapp.com",
+  projectId: "mailbox-96a99",
+  storageBucket: "mailbox-96a99.appspot.com",
+  messagingSenderId: "519903774413",
+  appId: "1:519903774413:web:9cf934da596365b399cb84",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
